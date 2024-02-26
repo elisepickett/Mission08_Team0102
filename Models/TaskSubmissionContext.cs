dc05d8f0d7de6cb2f2b8797db1d43f1d417e298d
pnie23 <pdnie567@gmail.com>
2024-02-26 11:04:07 -0700

    Fixed program.cs file to include dbcontext file

--- Models/TaskSubmissionContext.cs
+++ Models/TaskSubmissionContext.cs
@@ -19,7 +19,7 @@ namespace Mission08_Team0102.Models
                 new Category { CategoryId = 1, CategoryName = "Home" },
                 new Category { CategoryId = 2, CategoryName = "School" },
                 new Category { CategoryId = 3, CategoryName = "Work" },
-                new Category { CategoryId = 4, CategoryName = "Church" },
+                new Category { CategoryId = 4, CategoryName = "Church" }
 
             );
         }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TaskSubmissionContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings:TaskConnection"]);
+});
 
 var app = builder.Build();
 
