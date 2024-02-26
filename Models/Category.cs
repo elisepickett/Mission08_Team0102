f55414ff759be4cbd1d3868ab70445b4de97b8b8
pnie23 <pdnie567@gmail.com>
2024-02-26 09:52:05 -0700

    Fix Database Setup (still need connections in HomeController and Views)

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Team0102.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Team0102.Models
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+        [ForeignKey("CategoryId")]
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public int TaskName { get; set; }
+        public int Quadrant { get; set; }
+        public DateOnly? DueDate { get; set; }
+        public Boolean Completion { get; set; }
+    }
+}
