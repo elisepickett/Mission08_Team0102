8d923f199b419d28d112bc545f46b266ad2b044b
emkpeterson <emkpetersonn@gmail.com>
2024-02-26 23:31:54 -0700

    I have no idea if this is all set up right but layout.cshtml and AddTask and EditTask Views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,82 @@
 using Microsoft.AspNetCore.Mvc;
 using Mission08_Team0102.Models;
 using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Mission08_Team0102.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private TaskSubmissionContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(TaskSubmissionContext temp)
         {
-            _logger = logger;
+            _context = temp;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            ViewBag.Tasks = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+
+            return View("AddTask", new Task());
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(Task response) 
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tasks.Add(response); 
+                _context.SaveChanges();
+
+                return View("Index", response);
+            }
+            else //invalid data
+            {
+                ViewBag.Tasks = _context.Tasks
+                    .OrderBy(x => x.TaskName)
+                    .ToList();
+
+                return View(response);
+            }
+        }
+
+        public IActionResult EditTask()
+        {
+            var tasks = _context.Tasks
+                .OrderBy(x => x.TaskName).ToList();
+
+            return View(AddTask);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordToEdit = _context.Tasks
+                .Single(x => x.TaskId == id);
+
+            ViewBag.Majors = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+
+            return View("AddTask", recordToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task updatedInfo)
+        {
+            _context.Update(updatedInfo);
+            _context.SaveChanges();
+
+            return RedirectToAction("EditTask");
         }
     }
 }
