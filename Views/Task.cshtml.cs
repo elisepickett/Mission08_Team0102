320c07b3fbf51e6a4c89e015ab7e26adbdb9a007
emilypeterson <emkpetersonn@gmail.com>
2024-02-26 20:51:18 -0700

    clean up

---
+++ Views/Task.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mission08_Team0102.Views
+{
+    public class TaskModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
