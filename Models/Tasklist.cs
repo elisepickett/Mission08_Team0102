a1b8c0fc0130e84985e108053cf3c31b22132474
pnie23 <pdnie567@gmail.com>
2024-02-28 15:27:30 -0700

    Fixed random site css

--- Models/Tasklist.cs
+++ Models/Tasklist.cs
@@ -14,7 +14,7 @@ namespace Mission08_Team0102.Models
         public Category? Category { get; set; }
         [Required(ErrorMessage = "The Task Name Field is Required.")]
         public string TaskName { get; set; }
-        [Range(1, 4, ErrorMessage = "The Quadrant Field is Required.")]
+        [Range(1, 4, ErrorMessage = "The Quadrant Field is Required, and must be between 1-4.")]
         public int Quadrant { get; set; }
         public DateOnly? DueDate { get; set; }
         [Required(ErrorMessage = "The Completion Field is Required.")]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function clicked(e) {
+    if (!confirm('Confirm Task Submission?')) {
+        e.preventDefault();
+    }
+}
